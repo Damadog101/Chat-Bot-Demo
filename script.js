ea9d8f9de894e91568c36062861f61b0a01ef2b6
Ethan <116187182+Damadog101@users.noreply.github.com>
2023-03-17 10:43:10 -0700

    sdfdsfds

--- script.js
+++ script.js
@@ -10,29 +10,66 @@ submit.addEventListener("click", () => {
 
     
     output.appendChild(userText)
+    let botText = document.createElement("p")
 
     if (inputValue == "Rickroll Me") {
         console.log(inputValue)
 
-        let botText = document.createElement("p")
         botText.innerHTML = "Bot: https://www.youtube.com/watch?v=dQw4w9WgXcQ"
         output.appendChild(botText)
     } else if (inputValue == "potato") {
         console.log(inputValue)
-        let botText = document.createElement("p")
         let potatoImg = document.createElement("img")
         potatoImg.src = "https://cdn.mos.cms.futurecdn.net/iC7HBvohbJqExqvbKcV3pP.jpg"
         botText.innerHTML = "Bot: "
         output.appendChild(botText)
         output.appendChild(potatoImg)
     } else if (inputValue == "death") {
-        let botText = document.createElement("p")
         botText.innerHTML = "Bot: Yours will be soon"
         output.appendChild(botText)
     } else {
-        let botText = document.createElement("p")
-        botText.innerHTML = "Bot: Hello! I am dumb."
-        output.appendChild(botText)
+        let num = Math.ceil(Math.random() * 9)
+
+        switch(num) {
+            case 1:
+                botText.innerHTML = "Bot: Hello! I am dumb."
+                output.appendChild(botText)
+                break
+            case 2:
+                botText.innerHTML = "Bot: 2 + 2 is fish"
+                output.appendChild(botText)
+                break
+            case 3:
+                botText.innerHTML = "Bot: You're bad"
+                output.appendChild(botText)
+                break
+            case 4:
+                botText.innerHTML = "Bot: https://strategictictactoe.netlify.app/"
+                output.appendChild(botText)
+                break
+            case 5:
+                botText.innerHTML = "Bot: amongus"
+                output.appendChild(botText)
+                break
+            case 6:
+                botText.innerHTML = "Bot: merry christmss"
+                output.appendChild(botText)
+                break
+            case 7:
+                botText.innerHTML = "Bot: TBA"
+                output.appendChild(botText)
+                break
+            case 8:
+                botText.innerHTML = "Bot: TBA"
+                output.appendChild(botText)
+                break
+            case 9:
+                botText.innerHTML = "Bot: TBA"
+                output.appendChild(botText)
+                break
+        }
+
+
     }
 
 
