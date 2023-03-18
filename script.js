869d8cde2ffa66bb7a0faf9220c30e28558f24bb
Ethan <116187182+Damadog101@users.noreply.github.com>
2023-03-18 16:02:32 -0700

    fixed bug i think

--- script.js
+++ script.js
@@ -1,80 +1,79 @@
-let output = document.getElementById("output")
-let input = document.getElementById("input")
-let submit = document.getElementById("submit")
-
+let output = document.getElementById("output");
+let input = document.getElementById("input");
+let submit = document.getElementById("submit");
 
 submit.addEventListener("click", () => {
-    let inputValue = input.value;
-    let userText = document.createElement("p")
-    userText.innerHTML = "User: " + inputValue
+	let inputValue = input.value;
+	let userText = document.createElement("p");
+	userText.innerHTML = "User: " + inputValue;
 
-    
-    output.appendChild(userText)
-    let botText = document.createElement("p")
+	output.appendChild(userText);
+	let botText = document.createElement("p");
 
-    if (inputValue == "Rickroll Me") {
-        console.log(inputValue)
+	if (inputValue == "Rickroll Me") {
+		console.log(inputValue);
 
-        botText.innerHTML = "Bot: https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-        output.appendChild(botText)
-    } else if (inputValue == "potato") {
-        console.log(inputValue)
-        let potatoImg = document.createElement("img")
-        potatoImg.src = "https://cdn.mos.cms.futurecdn.net/iC7HBvohbJqExqvbKcV3pP.jpg"
-        botText.innerHTML = "Bot: "
-        output.appendChild(botText)
-        output.appendChild(potatoImg)
-    } else if (inputValue == "death") {
-        botText.innerHTML = "Bot: Yours will be soon"
-        output.appendChild(botText)
-    } else if (inputValue == "you bad") {
-botText.innerHTML = "Bot: So are you!"
-output.appendChild(botText)
-} else {
-        let num = Math.ceil(Math.random() * 9 )
+		botText.innerHTML = "Bot: https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+		output.appendChild(botText);
+	} else if (inputValue == "potato") {
+		console.log(inputValue);
+		let potatoImg = document.createElement("img");
+		potatoImg.src =
+			"https://cdn.mos.cms.futurecdn.net/iC7HBvohbJqExqvbKcV3pP.jpg";
+		botText.innerHTML = "Bot: ";
+		output.appendChild(botText);
+		output.appendChild(potatoImg);
+	} else if (inputValue == "death") {
+		botText.innerHTML = "Bot: Yours will be soon";
+		output.appendChild(botText);
+	} else if (inputValue == "you bad") {
+		botText.innerHTML = "Bot: So are you!";
+		output.appendChild(botText);
+	} else {
+		let num = Math.ceil(Math.random() * 9);
 
-        switch(num) {
-            case 1:
-                botText.innerHTML = "Bot: You're bad"
-                output.appendChild(botText)
-                break
-            case 2:
-                botText.innerHTML = "Bot: no"
-                output.appendChild(botText)
-                break
-            case 3:
-                botText.innerHTML = "Bot: yes"
-                output.appendChild(botText)
-                break
-            case 4:
-                botText.innerHTML = "Bot: aw hail naw"
-                output.appendChild(botText)
-                break
-            case 5:
-                botText.innerHTML = "Bot: Maybe"
-                output.appendChild(botText)
-                break
-            case 6:
-                botText.innerHTML = "Bot: tomato"
-                output.appendChild(botText)
-                break
-            case 7: 
-                botText.innerHTML = "Bot: ask again later"
-                break
-            case 8: 
-                botText.innerHTML = "Bot: we'll see"
-                break
-            case 9: 
-                botText.innerHTML = "Bot: god please no"
-                break     
-        }
-    }
+		switch (num) {
+			case 1:
+				botText.innerHTML = "Bot: You're bad";
+				output.appendChild(botText);
+				break;
+			case 2:
+				botText.innerHTML = "Bot: no";
+				output.appendChild(botText);
+				break;
+			case 3:
+				botText.innerHTML = "Bot: yes";
+				output.appendChild(botText);
+				break;
+			case 4:
+				botText.innerHTML = "Bot: aw hail naw";
+				output.appendChild(botText);
+				break;
+			case 5:
+				botText.innerHTML = "Bot: Maybe";
+				output.appendChild(botText);
+				break;
+			case 6:
+				botText.innerHTML = "Bot: tomato";
+				output.appendChild(botText);
+				break;
+			case 7:
+				botText.innerHTML = "Bot: ask again later";
+				output.appendChild(botText);
 
+				break;
+			case 8:
+				botText.innerHTML = "Bot: we'll see";
+				output.appendChild(botText);
 
+				break;
+			case 9:
+				botText.innerHTML = "Bot: god please no";
+				output.appendChild(botText);
 
+				break;
+		}
+	}
 
-
-
-    input.value = null
-})
-
+	input.value = null;
+});
